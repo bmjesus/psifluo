2c105fb2401a87c640a586aa5d6318b4e34e19ba
Bruno Jesus <bmjesus@gmail.com>
2018-05-21 12:47:30 -0300

    just a test to see if I could commit a change

--- R/psi_fluo.R
+++ R/psi_fluo.R
@@ -2,12 +2,12 @@
 #' @description  Main processing scrip for PSI fluorometer generated files. Script cycles through all FluorWin output files in a specified folder and calculates FRR paramaters.
 #' @param sti the user decides if fit should be done in the induction points
 #' @param str the user decides if fit should be done in the relaxation points
-#' @param sti_model decide which sti model to fit c("fofmsig","fofmsigp","sigp","sig"). "fofmsigp", which fits fo fm sigmaPSII and rho; "fofmsig", which fits fo fm 
+#' @param sti_model decide which sti model to fit c("fofmsig","fofmsigp","sigp","sig"). "fofmsigp", which fits fo fm sigmaPSII and rho; "fofmsig", which fits fo fm
 #' and sigmaPSII and sets rho to 0; "sigp", which fits sigmaPSII and p, and calculates fo and fm as the mean of points specified in fo_pts and fm_pts; "sig", which fits sigmaPSII and sets p to 0, calcualtes fo and fm as the mean of points specified in fo_pts and fm_pts
 #' @param str_model decide which str model to fit
 #' @param protocol the current supported protocols are Campbell and RLC
 #' @return list all the objects the function returns plus the output files
-#' @export 
+#' @export
 fit_model_fofm<-function(file_name,
                          out_name="results",
                          samples=1,
@@ -20,24 +20,24 @@ fit_model_fofm<-function(file_name,
                          protocol="Campbell"){
 
 # List of settings that will be used throughout the script
-settings=list() 
+settings=list()
 settings$run_sti<-sti
-settings$sti_model<-sti_model  
+settings$sti_model<-sti_model
 settings$PSI.Calibration.File <- calib_file
 # set the maximum value for Sigma PSII
 settings$sigma.PSII.max.lim <- 10000 #why do we need this?
 settings$run_str<-str
 settings$str_model<-str_model
+#
 
-
-###########################################  
+###########################################
 ##PARSING FUNCTION
 #Added a parameter to select the input file
 
 if (protocol=='RLC'){
-  
+
   read_psi(file_name,out_name,samples,num_steps,calib_file)
-  
+
 }else{
   psiworx.read(filename=file_name)
 }
@@ -67,24 +67,24 @@ nam<-out_name
 a<-grepl("AM|PM", time.data[,3])
 
 if (a[1]==FALSE){
-  formatted.time <- strptime(time.data[,3], format="%d/%m/%Y %H:%M:%S")  
+  formatted.time <- strptime(time.data[,3], format="%d/%m/%Y %H:%M:%S")
 }else{
-  formatted.time <- strptime(time.data[,3], format="%m/%d/%Y %I:%M:%S %p")  
+  formatted.time <- strptime(time.data[,3], format="%m/%d/%Y %I:%M:%S %p")
 }
 
 #print(formatted.time)
 
 #Load PSI light level calibration .csv file - we need to make this!! ##################******************$$############!!!!!!!!!!!!!!!!!!!!!
-#BJ: why is this file imported twice? here and in settings$PSI.Calibration.File 
+#BJ: why is this file imported twice? here and in settings$PSI.Calibration.File
 cali <- read.csv(calib_file,header=T)
 cali<-assign("cali",cali, envir = e2)
 
 #Extract the power level from the calibration table based on meta data flashlet voltage.
 #it comes from the function pw_level()
-   
+
 power<-pw_level(protocol)
 
-#print(power)    
+#print(power)
 ############################################ Fit STI
 
 # Power per flash is determined from the calibration .csv and is specific to instument used
@@ -99,7 +99,7 @@ names(sti.fit)=sti.fit.names[order(sti.fit.names)]
 rm(sti.fit.names)
 
 # open blank .pdf file where all sti plots will be saved, each focus file whill have an individual .pdf output - this uses the output name previously input by the user at the beginning of the script.
-#BJ: include a parameter to activate/desactivate the pdf output (true by default) 
+#BJ: include a parameter to activate/desactivate the pdf output (true by default)
 pdf(paste(nam,"_sti_fit.pdf",sep=""))
 
 # loop through each column of fluorescence data and apply the STI model####
@@ -112,9 +112,9 @@ for(c in 2:ncol(data.sti)){ #note we run from 2nd column this avoiding first col
     titl<-names(data.sti)[c]
 
 ###################################
-####FITTING FUNCTION 
-fit_sti(x,y,fo.opt,fm.opt,sigma.opt,rho.opt,sigma_max,wm,sti.fit,c)        
-    
+####FITTING FUNCTION
+fit_sti(x,y,fo.opt,fm.opt,sigma.opt,rho.opt,sigma_max,wm,sti.fit,c)
+
 #Output object needed for the plotting function
 #it is stored in the Environment e2
 res<-local(res, env = e2)
@@ -125,9 +125,9 @@ names(sti.fit)<-names(res)
 sti.fit[c-1,]<-res
 
 ###################################
-####PLOTING FUNCTION 
-psi_plots(y, fit1,fit1u,fit1l,titl,res)  
-  
+####PLOTING FUNCTION
+psi_plots(y, fit1,fit1u,fit1l,titl,res)
+
 
 }
 
@@ -138,7 +138,7 @@ dev.off() # puts plots in .pdf created, adobe cannot be open
 
 ################################################################################
 #screen version
-#temporary fix to plot the sti on the screen, this is far from being ideal 
+#temporary fix to plot the sti on the screen, this is far from being ideal
 #we are repeating the same calculations and fits that were previously done for the pdf
 #maybe it would be better to first create an object with all the data needed for the
 #plottings and then do one pdf and one screen version. The non-fitted would be
@@ -159,8 +159,8 @@ for(c in 2:ncol(data.sti)){ #note we run from 2nd column this avoiding first col
   sigma_max<-settings$sigma.PSII.max.lim
   titl<-names(data.sti)[c]
   ###################################
-  ####FITTING FUNCTION 
-  fit_sti(x,y,fo.opt,fm.opt,sigma.opt,rho.opt,sigma_max,wm,sti.fit,c)        
+  ####FITTING FUNCTION
+  fit_sti(x,y,fo.opt,fm.opt,sigma.opt,rho.opt,sigma_max,wm,sti.fit,c)
   #Output object needed for the plotting function
   #it is stored in the Environment e2
   res<-local(res, env = e2)
@@ -169,8 +169,8 @@ for(c in 2:ncol(data.sti)){ #note we run from 2nd column this avoiding first col
   #add res into sti.fit data frame in appropriate row
   sti.fit[c-1,]<-res
   ###################################
-  ####PLOTING FUNCTION 
-  psi_plots_screen(y, fit1,fit1u,fit1l,titl,res)  
+  ####PLOTING FUNCTION
+  psi_plots_screen(y, fit1,fit1u,fit1l,titl,res)
 }
 #dev.off()
 #par(def.par)
@@ -180,7 +180,7 @@ for(c in 2:ncol(data.sti)){ #note we run from 2nd column this avoiding first col
 
 #checking if the user also wants to plot the relaxation step
 if (settings$run_str == TRUE) {
-  
+
 ################################## FIT the STR model ####################################
 
 # open blank .pdf file where all plots will be saved, each focus file whill have an individual .pdf output
@@ -193,7 +193,7 @@ pdf(file=paste(nam,"_str_fit.pdf",sep=""))
     names(str.fit)=str.fit.names[order(str.fit.names)]
     rm(str.fit.names)
 
-   
+
 #loop through columns (starting at second to avoid the time column) of data.str i.e. the relaxation curves
 for(d in 2:ncol(data.str)){
         #define our new x, y etc for str model inputs
@@ -214,11 +214,11 @@ x<-x/1e6;
   fg$alpha2.opt=1-fg$alpha1.opt;
   fg$tau3.opt=fg$tau1.opt*2;
 
-    
+
 ub<-c(fo.opt=Inf,fm.opt=Inf,tau1.opt=Inf,alpha1.opt=1,tau2.opt=Inf,alpha2.opt=1,tau3.opt=Inf);
 lb<-c(fo.opt=-Inf,fm.opt=-Inf,tau1.opt=-Inf,alpha1.opt=0,tau2.opt=-Inf,alpha2.opt=0,tau3.opt=-Inf);
 
-#fitting str function 
+#fitting str function
 fit_str(x,y,fo.opt = fg$fo.opt,fm.opt=fg$fm.opt,wm,d,fg=fg,ub=ub,lb=lb)
 
 #Output object needed for the plotting function
@@ -226,8 +226,8 @@ fit_str(x,y,fo.opt = fg$fo.opt,fm.opt=fg$fm.opt,wm,d,fg=fg,ub=ub,lb=lb)
 #it would be nice to change the name of this object so that it's different
 #from the sti function
 res<-local(res, env = e2)
-    
-    
+
+
 res$model_str=wm;
 names(str.fit)<-names(res)
 #add res into str.fit data frame in appropriate row
@@ -244,8 +244,8 @@ titl<-names(data.str)[d]
 
 #plot str function
 psi_plots_str(x,y, fit1,fit1u,fit1l,titl,res)
-      
-      
+
+
 #remove objects to prevent carry over
 #rm(x)
 #rm(y)
@@ -259,7 +259,7 @@ dev.off() # puts plots in .pdf created, adobe cannot be open
 
 ################################################################################
 ##Screen version
-#temporary fix to plot the STR on the screen, this is far from being ideal 
+#temporary fix to plot the STR on the screen, this is far from being ideal
 #we are repeating the same calculations and fits that were previously done for the pdf
 #maybe it would be better to first create an object with all the data needed for the
 #plottings and then do one pdf and one screen version. The non-fitted would be
@@ -277,10 +277,10 @@ for(d in 2:ncol(data.str)){
   x<-data.str$time
   y<-data.str[,d]
   wm<-settings$str_model
-  
+
   # change time to seconds
   x<-x/1e6;
-  
+
   # First guesses and limits for relaxation fit
   fg=list();
   fg$fo.opt=min(y);
@@ -290,27 +290,27 @@ for(d in 2:ncol(data.str)){
   fg$tau2.opt=fg$tau1.opt/10;
   fg$alpha2.opt=1-fg$alpha1.opt;
   fg$tau3.opt=fg$tau1.opt*2;
-  
-  
+
+
   ub<-c(fo.opt=Inf,fm.opt=Inf,tau1.opt=Inf,alpha1.opt=1,tau2.opt=Inf,alpha2.opt=1,tau3.opt=Inf);
   lb<-c(fo.opt=-Inf,fm.opt=-Inf,tau1.opt=-Inf,alpha1.opt=0,tau2.opt=-Inf,alpha2.opt=0,tau3.opt=-Inf);
-  
-  #fitting str function 
+
+  #fitting str function
   fit_str(x,y,fo.opt = fg$fo.opt,fm.opt=fg$fm.opt,wm,d,fg=fg,ub=ub,lb=lb)
-  
+
   #Output object needed for the plotting function
   #it is stored in the Environment e2
   #it would be nice to change the name of this object so that it's different
   #from the sti function
   res<-local(res, env = e2)
-  
-  
+
+
   res$model_str=wm;
   names(str.fit)<-names(res)
   #add res into str.fit data frame in appropriate row
   str.fit[d-1,]<-res
-  
-  
+
+
   #make str plots that will write to already opened pdf file.
   #define parameters again here to make certain they are correct
   x<-data.str$time/1e6; # change time to seconds
@@ -318,11 +318,11 @@ for(d in 2:ncol(data.str)){
   wplot<-'str'
   res<-str.fit[d-1,]
   titl<-names(data.str)[d]
-  
+
   #plot str function
   psi_plots_str_screen(x,y, fit1,fit1u,fit1l,titl,res)
-  
-  
+
+
 
 } #end of str loop through data.str columns
 dev.set()
@@ -362,8 +362,8 @@ if(rep_names[1]!=F){
   } else {
     results<-cbind(lc_in_file, step_in_lc, formatted.time, sti.fit)
     names(results)[1:3]<-c('lc_in_file','step_in_lc','measurement_time')
-  }  
-  
+  }
+
 }
 
 
